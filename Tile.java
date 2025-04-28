e2681db943aee279a2bac076920c1b2d4d9e0bd9
RideZaWind <114116286+RideZaWind@users.noreply.github.com>
2025-04-27 19:23:31 -0700

    Update Tile.java
    Added features for Items

--- Tile.java
+++ Tile.java
@@ -1,8 +1,14 @@
 public class Tile {
     private TerrainType terrain;
+    private int food, water, gold;
+    private boolean repeatableBonus;
+    // private Trader trader;
 
     public Tile(TerrainType terrain) {
         this.terrain = terrain;
+        food = 0; water = 0; gold = 0;
+        repeatableBonus = false;
+        // trader = null;
     }
 
     public TerrainType getTerrain() {
@@ -12,4 +18,34 @@ public class Tile {
     public void setTerrain(TerrainType terrain) {
         this.terrain = terrain;
     }
+
+    public void setBonuses(int foodBonus, int waterBonus, int goldBonus, boolean repeatable) {
+        food = foodBonus;
+        water = waterBonus;
+        gold = goldBonus;
+        repeatableBonus = repeatable;
+    }
+
+    public void setBonuses(int foodBonus, int waterBonus, int goldBonus) {
+        food = foodBonus;
+        water = waterBonus;
+        gold = goldBonus;
+    }
+
+    public int getFood() { 
+        return food; 
+    }
+
+    public int getWater() { 
+        return water; 
+    }
+
+    public int getGold() { 
+        return gold; 
+    }
+
+    // public boolean hasTrader() {
+    //     return trader == null;
+    // }
+
 }
