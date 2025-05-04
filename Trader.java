11e82cba99741456df6357694364284ec72d9d9b
Makinzi Nagashima <Makinzijc@gmail.com>
2025-05-03 17:07:26 -0700

    Update Trader.java

--- Trader.java
+++ Trader.java
@@ -6,6 +6,7 @@ public abstract class Trader {
     private int water;
     private int gold;
     private boolean isTrading;
+    private int change;
     private final Random random = new Random();
 
     public Trader(){
@@ -13,14 +14,16 @@ public abstract class Trader {
         this.food = 0;
         this.water = 0;
         this.gold = 0;
+        this.change = 1;
         isTrading = true;
     }
     
-    public Trader(int patienceLevel, int food, int water, int gold){
+    public Trader(int patienceLevel, int food, int water, int gold, int change){
         this.patienceLevel = patienceLevel;
         this.food = food;
         this.water = water;
         this.gold = gold;
+        this.change = change;
         isTrading = true;
     }
 
@@ -35,7 +38,7 @@ public abstract class Trader {
             weight+= offer.getOfferWater()-offer.getWantWater();
             weight+= offer.getOfferGold()-offer.getWantGold();
             
-            successful = roll(weight);
+            successful = roll(weight, "default");
 
             if(successful){
                 trade=new Offer();
@@ -45,10 +48,10 @@ public abstract class Trader {
             } else {
                 trade = new Offer();
                 while(weight<=3){
-                    trade.setOfferFood(offer.getWantFood()-offer.getOfferFood()-1);
-                    trade.setOfferWater(offer.getWantWater()-offer.getOfferWater()-1);
-                    trade.setOfferGold(offer.getWantGold()-offer.getOfferGold()-1);
-                    weight+=3;
+                    trade.setOfferFood(offer.getWantFood()-offer.getOfferFood()-change);
+                    trade.setOfferWater(offer.getWantWater()-offer.getOfferWater()-change);
+                    trade.setOfferGold(offer.getWantGold()-offer.getOfferGold()-change);
+                    weight+=3*change;
                 }
             }
             
@@ -62,23 +65,55 @@ public abstract class Trader {
         return trade;
     }
 
-    private boolean roll( int weight){
+    private boolean roll( int weight, String type){
         int roll = random.nextInt(100);
         boolean successful = false;
-        if(-1 <= weight && weight < 2){
-            if(roll>=40){
+        int firstLower = -1;
+        int firstUpper = 2;
+        int secondLower = 2;
+        int secondUpper = 4;
+        int ThirdUpper = 4;
+        int firstRoll= 40;
+        int secondRoll= 10;
+        int patienceMinus = 1;
+
+        switch(type){
+            case "Cheap": 
+                firstLower = 0;
+                firstUpper = 3;
+                secondLower = 3;
+                secondUpper = 5;
+                ThirdUpper = 5;
+                firstRoll= 60;
+                secondRoll= 40;
+                break;
+            case "Expensive": 
+                firstLower = 1;
+                firstUpper = 4;
+                secondLower = 4;
+                secondUpper = 6;
+                ThirdUpper = 6;
+                break;
+            case "Impatient": 
+                patienceMinus=2;
+                break;
+            default: break;
+        }  
+
+        if(firstLower <= weight && weight < firstUpper){
+            if(roll>=firstRoll){
                 successful=true;
             } else {
-                patienceLevel--;
+                patienceLevel-=patienceMinus;
             }
-        } else if(2 <= weight && weight < 4){
-            if(roll>=10){
+        } else if(secondLower <= weight && weight < secondUpper){
+            if(roll>=secondRoll){
                 successful=true;
             }
-        } else if(weight >= 4){
+        } else if(weight >= ThirdUpper){
             successful=true;
         } else {
-            patienceLevel--;
+            patienceLevel-=patienceMinus;
         }
         return successful;
     }
