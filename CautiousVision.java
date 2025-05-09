d7802d737eefd5e6f51c983ef8db47d692c46c6b
RideZaWind <114116286+RideZaWind@users.noreply.github.com>
2025-05-08 22:57:51 -0700

    Update CautiousVision.java
    Update to accommodate for Vision changes

--- CautiousVision.java
+++ CautiousVision.java
@@ -1,76 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class CautiousVision extends Vision {
 
     public CautiousVision(Player player, WSSMap map) {
         super(player, map);
-    }
-
-    @Override
-    public Path closestWater() {
-        return findClosest("water");
-    }
-
-    @Override
-    public Path closestFood() {
-        return findClosest("food");
-    }
-
-    @Override
-    public Path closestGold() {
-        return findClosest("gold");
-    }
-
-    @Override
-    public Path easiestPath() {
-        return findLowestCost();
-    }
-
-    private Path findClosest(String resource) {
-      List<Direction> directions = List.of(Direction.EAST, Direction.NORTH, Direction.SOUTH);
-        for (Direction dir : directions) {  //scans tiles in cardinal directions
-            Tile tile = map.getTileInDirection(dir); //retrieves specified tile object
-            if (tile != null && hasResource(tile, resource)) {
-                return new Path(List.of(dir),
-                        tile.getTerrain().getMoveCost(),
-                        tile.getTerrain().getWaterCost(),
-                        tile.getTerrain().getFoodCost());
-            }
-        }
-        return null;
-    }
-
-    //returns item bonus on a tile, if any
-    private boolean hasResource(Tile tile, String resource) {
-        return switch (resource) {
-            case "water" -> tile.getWater() > 0;
-            case "food" -> tile.getFood() > 0;
-            case "gold" -> tile.getGold() > 0;
-            default -> false;
+        offsets = new int[][] {
+            {1, 0},     // SOUTH
+            {-1, 0},    // NORTH
+            {0, 1}      // EAST
         };
-    }
-
-    //returns coordinates and cost of moving to the tile with the lowest traversal cost
-    //in adjacent cardinal directions.  TODO: consider ordinal directions? (NORTHEAST, NORTHWEST, etc)
-    private Path findLowestCost() {
-        List<Direction> directions = List.of(Direction.EAST, Direction.NORTH, Direction.SOUTH);
-        Path best = null;
-        int bestCost = Integer.MAX_VALUE;
-
-        for (Direction dir : directions) {
-            Tile tile = map.getTileInDirection(dir);
-            if (tile != null) {
-                int cost = tile.getTerrain().getMoveCost();
-                if (cost < bestCost) {
-                    bestCost = cost;
-                    best = new Path(List.of(dir),
-                            tile.getTerrain().getMoveCost(),
-                            tile.getTerrain().getWaterCost(),
-                            tile.getTerrain().getFoodCost());
-                }
-            }
-        }
-        return best;
     }
 }
